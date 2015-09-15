bf8c7944533ac33d82a6897f640a7765291620f6
SpazzMarticus <SpazzMarticus@users.noreply.github.com>
2015-09-15 11:22:12 +0200

    Added tests and configuration for tests and phpdocumentor documentation

---
+++ tests/unit/eCSStractorTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SpazzMarticus\eCSStractor;
+
+/**
+ * @uses \SpazzMarticus\eCSStractor\eCSStractor
+ * @covers \SpazzMarticus\eCSStractor\eCSStractor::__construct
+ */
+class eCSStractorTest extends \PHPUnit_Framework_TestCase
+{
+    protected $_eCSStractor;
+
+    protected function setUp()
+    {
+        $this->_eCSStractor=new eCSStractor();
+    }
+
+    /**
+     * @dataProvider providerExtract
+     * @covers \SpazzMarticus\eCSStractor\eCSStractor::extract
+     */
+    public function testExtract($html, $cssFormat)
+    {
+        $this->assertStringMatchesFormat($cssFormat, $this->_eCSStractor->extract($html));
+    }
+
+    public function providerExtract()
+    {
+        $path=__DIR__.'/../fixtures/';
+        return [
+            ['plaintext', ''],
+            [123.2123, ''],
+            [file_get_contents($path.'01.html'), $this->loadFormat($path.'01.css')],
+            [file_get_contents($path.'02.html'), $this->loadFormat($path.'02.css')],
+            [file_get_contents($path.'03.html'), $this->loadFormat($path.'03.css')],
+        ];
+    }
+
+    public function loadFormat($file)
+    {
+        $format=file_get_contents($file);
+        if (!$format)
+        {
+            return false;
+        }
+        return '%w'.preg_replace('/\s/', '%w', $format).'%w';
+    }
+}
